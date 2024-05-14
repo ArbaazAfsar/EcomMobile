cdf2a1bba56873d48e7b36d15a051a7d55213f91
Arbaaz Afsar <arbaazbinafsar@gmail.com>
2024-05-14 15:26:03 +0530

    Update upset layout  In products, brands, Header quotes and images.

--- EcomMobilPro/Products/urls.py
+++ EcomMobilPro/Products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
+    path('products-list', views.productsLists, name= 'productds_List')
       
 ]
--- EcomMobilPro/Products/views.py
+++ EcomMobilPro/Products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,get_object_or_404
 from .models import brand,product
 
 # Create your views here.
@@ -7,6 +7,14 @@ def index(request):
     brands = brand.objects.all()
     mobiles = product.objects.all()
     return render(request, 'index.html', {'brands':brands, 'mobiles': mobiles})
+
+def productsLists(request):
+    mobiles = product.objects.all()
+    return render(request, 'productsList.html', { 'mobiles': mobiles})
+
+def BrandMob(request, Brand_name):
+    pass
+    
 
 
 
