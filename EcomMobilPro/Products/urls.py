c2dbff1745c3925f34b5ada6cd7e8a2a0984ad78
Arbaaz Afsar <arbaazbinafsar@gmail.com>
2024-05-15 18:01:44 +0530

    I created products by brand, And create brand.html file

--- EcomMobilPro/Products/urls.py
+++ EcomMobilPro/Products/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
-    path('products-list', views.productsLists, name= 'productds_List')
+    path('products-list', views.productsLists, name= 'productds_List'),
+    path('brand/<str:Brand_name>', views.BrandMob, name = 'brandMob'),
+    path('base', views.base)
+    
       
 ]
--- EcomMobilPro/Products/views.py
+++ EcomMobilPro/Products/views.py
@@ -9,12 +9,27 @@ def index(request):
     return render(request, 'index.html', {'brands':brands, 'mobiles': mobiles})
 
 def productsLists(request):
+    brands = brand.objects.all()
     mobiles = product.objects.all()
-    return render(request, 'productsList.html', { 'mobiles': mobiles})
+    return render(request, 'productsList.html', {'brands':brands,  'mobiles': mobiles})
+
+def base(request):
+    brands = brand.objects.all()
+    return render(request, 'base.html', {'brands':brands} )
+    
+
 
 def BrandMob(request, Brand_name):
-    pass
-    
+    error= None
+    try:
+        brands = product.objects.filter(Brand_name__Brand_name=Brand_name)
+        brnd = brand.objects.all()
+        
+        return render(request, 'brand.html', {'brands': brands, 'brnd':brnd})
+        
+    except product.DoesNotExist:
+        error = {'error':"Brand not found"}
+        return render(request,'brand.html', error )
 
 
 
