2449a6e464f570ab42dfba50154467b4e2e23afa
Arbaaz Afsar <arbaazbinafsar@gmail.com>
2024-05-16 17:29:15 +0530

    Created a product details page

--- EcomMobilPro/Products/urls.py
+++ EcomMobilPro/Products/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index' ),
     path('products-list', views.productsLists, name= 'productds_List'),
-    path('brand/<str:Brand_name>', views.BrandMob, name = 'brandMob'),
-    path('base', views.base)
+    path('brand/<str:Brand_name>', views.brand_mob, name = 'brandMob'),
+    path('base', views.base),
+    path('product-details/<int:pk>,<str:Brand_name>', views.productDetails, name = 'products_details'),
+    
     
       
 ]
--- EcomMobilPro/Products/views.py
+++ EcomMobilPro/Products/views.py
@@ -19,17 +19,26 @@ def base(request):
     
 
 
-def BrandMob(request, Brand_name):
-    error= None
-    try:
-        brands = product.objects.filter(Brand_name__Brand_name=Brand_name)
-        brnd = brand.objects.all()
-        
-        return render(request, 'brand.html', {'brands': brands, 'brnd':brnd})
-        
-    except product.DoesNotExist:
-        error = {'error':"Brand not found"}
-        return render(request,'brand.html', error )
+
+
+def brand_mob(request, Brand_name):
+    message = None
+    brands = product.objects.filter(Brand_name__Brand_name=Brand_name)
+    if brands.exists():
+        all_brands = brand.objects.all()
+        return render(request, 'brand.html', {'brands': brands, 'all_brands': all_brands})
+    else:
+        all_brands = brand.objects.all()
+        message = "This product is not available in this time"
+        return render(request, 'brand.html',  {'message':message,'all_brands': all_brands})
+
+    
+    
+def productDetails(request, pk, Brand_name):
+    mobile = product.objects.get( id = pk)
+    brands = product.objects.filter(Brand_name__Brand_name=Brand_name)
+    brnd= brand.objects.all()
+    return render(request, 'product_details.html', {'mobile':mobile, 'brands': brands, 'brnd':brnd})
 
 
 
